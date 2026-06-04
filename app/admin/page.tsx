15009142f63b46af47cf29308c4cbe0cd4877f86
gzlcg23 <gonzalo.cg.23@gmail.com>
2026-06-04 02:33:15 -0600

    ajuste filtro archivados

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,5 @@
 // app/admin/page.tsx
+export const dynamic = 'force-dynamic'; // 🌟 Desactiva el caché para esta ruta de administración
 import { prisma } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";

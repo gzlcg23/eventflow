18bf1238c1eb1729b990e145d83a4fbe9d050125
gzlcg23 <gonzalo.cg.23@gmail.com>
2026-06-04 16:03:38 -0600

    error footer

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 // app/dashboard/layout.tsx
-import Footer from "../components/Footer"; // Verifica si en VS Code te pide un punto "." o dos ".."
 
 export default function DashboardLayout({
   children,
@@ -16,7 +15,7 @@ export default function DashboardLayout({
       </div>
 
       {/* 🌟 Tu footer profesional claro al fin se renderizará abajo del dashboard */}
-      <Footer variant="light" />
+     
     </div>
   );
 }
